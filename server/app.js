e523b88c0c822faf69e558c00b43101d59aaea26
Trevor Old <trevor.old@outlook.com>
2018-03-05 15:29:36 -0800

    rendering of personal component and loading fonts

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+var proxy = require('http-proxy-middleware');
+
+
+const PORT = 3002;
+const app = express();
+app.use(express.static('public'));
+
+app.use('/v1/*', proxy({target:'http://localhost:3003/', changeOrigin:true}));
+app.use('/fonts/*', proxy({target:'http://localhost:3003/', changeOrigin:true}));
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`);
+})

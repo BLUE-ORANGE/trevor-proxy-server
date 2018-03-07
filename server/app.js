dabdacbfca3dba3ec856efdd1814c60037c56fb7
Trevor Old <trevor.old@outlook.com>
2018-03-06 18:18:58 -0800

    added each members component

--- server/app.js
+++ server/app.js
@@ -6,7 +6,11 @@ const PORT = 3000;
 const app = express();
 app.use(express.static('public'));
 
-app.use('/v1/*', proxy({target:'http://localhost:3003/', changeOrigin:true}));
+app.use('/v1/product/*/images', proxy({target:'http://localhost:3003/', changeOrigin:true}));
+
+app.use('/v1/products/*', proxy({target:'http://localhost:3009/', changeOrigin:true}));
+app.use('/v1/shop*', proxy({target:'http://localhost:3009/', changeOrigin:true}));
+
 app.use('/fonts/*', proxy({target:'http://localhost:3003/', changeOrigin:true}));
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);

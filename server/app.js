097cad236aee5628e861406d1968f2040d4ab885
Trevor Old <trevor.old@outlook.com>
2018-03-08 10:27:49 -0800

    add styling and config

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 var proxy = require('http-proxy-middleware');
-
+const config = require('../configuration/index');
+const log = require('../configuration/log')(module);
 
 const PORT = 3000;
 const app = express();
+const router = express.Router();
 app.use(express.static('public'));
+
 
 app.use('*/images', proxy({target:'http://localhost:3003/', changeOrigin:true}));
 
@@ -12,6 +15,7 @@ app.use('/v1/product/*', proxy({target:'http://localhost:3004/', changeOrigin:tr
 app.use('*shop*', proxy({target:'http://localhost:3009/', changeOrigin:true}));
 
 app.use('/fonts/*', proxy({target:'http://localhost:3003/', changeOrigin:true}));
-app.listen(PORT, () => {
-  console.log(`listening on port: ${PORT}`);
+app.set('port', config.get('port'));
+app.listen(app.get('port'), () => {
+  log.info(`listening on port: ${config.get('port')}`);
 })

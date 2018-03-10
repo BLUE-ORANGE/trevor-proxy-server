7250d16fc831759ac3ee570e942f74a80e535dc2
Trevor Old <trevor.old@outlook.com>
2018-03-09 18:56:36 -0800

    docker

--- server/app.js
+++ server/app.js
@@ -1,17 +1,15 @@
 const express = require('express');
 var proxy = require('http-proxy-middleware');
+require('dotenv').config();
 
 
-const PORT = 3000;
 const app = express();
 app.use(express.static('public'));
 
-app.use('*/images', proxy({target:'http://localhost:3003/', changeOrigin:true}));
+// app.use('/*/images', proxy({target:'http://localhost:8001/', changeOrigin:true}));
 
-app.use('/v1/product/*', proxy({target:'http://localhost:3004/', changeOrigin:true}));
-app.use('*shop*', proxy({target:'http://localhost:3009/', changeOrigin:true}));
+// app.use('/v1/product/*/images', proxy({target:'http://localhost:8001/', changeOrigin:true}));
+// app.use('*shop*', proxy({target:'http://localhost:3009/', changeOrigin:true}));
 
-app.use('/fonts/*', proxy({target:'http://localhost:3003/', changeOrigin:true}));
-app.listen(PORT, () => {
-  console.log(`listening on port: ${PORT}`);
-})
+// app.use('/fonts/*', proxy({target:'http://localhost:3003/', changeOrigin:true}));
+app.listen(process.env.PORT, () =>   console.log(`listening on port: ${process.env.PORT}`))
